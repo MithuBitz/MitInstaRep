e6514bb607357fcc38e101e5eb52b9d5e5985c35
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2021-10-22 23:53:52 +0530

    -- SignUp and Login implementation

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,6 +11,12 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -26,55 +32,91 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener{
+
+    Boolean signUpModeActive = true;
+    TextView loginTV;
+    EditText usernameET;
+    EditText passwordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        //Create a user on the parse server
-//        ParseUser user = new ParseUser();
-//        //Set the username and password
-//        user.setUsername("mithu");
-//        user.setPassword("password");
-//        //SignUp the user
-//        user.signUpInBackground(new SignUpCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if (e == null) {
-//                    //All is well
-//                    Log.i("SignUp", "All is going well");
-//                } else {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
+        loginTV = findViewById(R.id.logInTV);
+        loginTV.setOnClickListener(this);
 
+        usernameET = findViewById(R.id.usernameET);
+        passwordET = findViewById(R.id.passwordET);
 
-       // Log in the user on the parse server
-//        ParseUser.logInInBackground("mithu", "password", new LogInCallback() {
-//            @Override
-//            public void done(ParseUser user, ParseException e) {
-//                if (e == null && user != null) {
-//                    Log.i("LogIn", "User Logged in Successfully");
-//                } else {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
+        //set the onKey listener for password field
 
-        //Log out the user from the parse server
-        ParseUser.logOut();
-
-        //If the user is logged in than no need to logged in again
-        if (ParseUser.getCurrentUser() != null) {
-            Log.i("UserActive", ParseUser.getCurrentUser().getUsername());
-        } else {
-            Log.i("InActive", "OOps the user not logged in");
-        }
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
 
+    public void signUpClicked(View view) {
+
+
+        if (usernameET.getText().toString().matches("") || passwordET.getText().toString().matches("")) {
+            Toast.makeText(this, "Username and Password Required!", Toast.LENGTH_SHORT).show();
+        } else {
+            if (signUpModeActive) {
+                //SignUp
+                ParseUser user = new ParseUser();
+                user.setUsername(usernameET.getText().toString());
+                user.setPassword(passwordET.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("SignUp", "Success");
+                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                //Login
+                ParseUser.logInInBackground(usernameET.getText().toString(), passwordET.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            Log.i("Login", "Successfully login");
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.logInTV) {
+
+            Button signUpBtn = findViewById(R.id.primaryButton);
+
+            if (signUpModeActive) {
+                signUpModeActive = false;
+                signUpBtn.setText("Login");
+                loginTV.setText("or, Sign Up");
+            } else {
+                signUpModeActive = true;
+                signUpBtn.setText("Sign Up");
+                loginTV.setText("or, Login");
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        return false;
+    }
 }
